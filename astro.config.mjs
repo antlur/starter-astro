2a73cfa5513a5b43adcf0c4df2bcb15f1b465ede
Anthony Holmes <anthony@antlur.co>
2024-11-11 22:13:23 -0600

    Astro, React, Netlify, ShadCN UI, Tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,29 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+
+import tailwind from "@astrojs/tailwind";
+
+import react from "@astrojs/react";
+
+import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  server: {
+    host: "0.0.0.0",
+    port: 3000,
+  },
+  integrations: [
+    tailwind({
+      applyBaseStyles: false,
+    }),
+    react(),
+  ],
+  output: "server",
+  adapter: netlify({
+    edgeMiddleware: true,
+  }),
+  image: {
+    domains: ["media.bckstg.app"],
+  },
+});
