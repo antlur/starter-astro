b10a7990e2d5495361f5ef8c33ff136ac5131bd7
Anthony Holmes <anthony@antlur.co>
2024-11-27 23:20:07 -0600

    add person context to App.Locals interface in middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,5 +1,16 @@
 import { defineMiddleware } from "astro:middleware";
 
+declare global {
+  namespace App {
+    interface Locals {
+      person: {
+        name: string;
+        age: number;
+      };
+    }
+  }
+}
+
 // `context` and `next` are automatically typed
 export const onRequest = defineMiddleware((context, next) => {
   context.locals.person = {
