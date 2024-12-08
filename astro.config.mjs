00bd0197004ae916a1242ef0fc620d76dae9bbe9
Anthony Holmes <anthony@antlur.co>
2024-12-08 14:12:06 -0600

    Disable edgeMiddleware in Netlify adapter configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -21,7 +21,7 @@ export default defineConfig({
   ],
   output: "server",
   adapter: netlify({
-    edgeMiddleware: true,
+    // edgeMiddleware: true,
   }),
   image: {
     domains: ["media.bckstg.app"],
