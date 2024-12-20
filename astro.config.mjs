b40b1c4c215c79e6ea6abfcce2e5840104ef5a74
Anthony Holmes <anthony@antlur.co>
2024-12-19 20:40:31 -0600

    Update Astro to 5.1

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,7 @@
 // @ts-check
 import { defineConfig } from "astro/config";
-
 import tailwind from "@astrojs/tailwind";
-
 import react from "@astrojs/react";
-
 import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
